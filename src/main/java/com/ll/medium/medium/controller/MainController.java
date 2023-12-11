64abb270bd4fe3122ac4b3dbccc92a358b856ac6
solsolsole <dmsthf917@naver.com>
2023-12-11 16:26:50 +0900

    회원가입 기능구현

--- src/main/java/com/ll/medium/medium/controller/MainController.java
+++ src/main/java/com/ll/medium/medium/controller/MainController.java
@@ -2,14 +2,13 @@ package com.ll.medium.medium.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class MainController {
 
-    @GetMapping("/sbb")
-    @ResponseBody
-    public String start () {
-        return "start";
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/board/list";
     }
 }
---
+++ src/main/java/com/ll/medium/medium/global/user/UserCreateForm.java
@@ -0,0 +1,24 @@
+package com.ll.medium.medium.global.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class UserCreateForm {
+    @Size(min = 3, max = 25)
+    @NotEmpty(message = "ID를 입력해주세요.")
+    private String username;
+
+    @NotEmpty(message = "비밀번호를 입력해주세요.")
+    private String password1;
+
+    @NotEmpty(message = "비밀번호 확인은 필수 항목입니다.")
+    private String password2;
+
+    @NotEmpty(message = "이메일을 입력해주세요.")
+    @Email
+    private String email;
+
+}
